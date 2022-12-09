c0cf6a9aafdaf4745da3d977fc15324731f6780d
Pavel Dedik <dedikx@gmail.com>
2022-12-09 16:12:26 +0100

    day 9 - cleanup

--- src/day9.rs
+++ src/day9.rs
@@ -1,172 +1,149 @@
-use std::iter::repeat;
+use std::{fmt, iter::repeat};
 
 use crate::common::read_file;
 
-enum Instruction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-type Playground = Vec<Vec<u8>>;
 type Position = (usize, usize);
 
-fn init_playground(size: usize) -> Playground {
-    Vec::from_iter((0..size).map(|_| vec![0; size]))
+struct Playground {
+    data: Vec<Vec<u8>>,
+    knots: Vec<Position>,
 }
 
-#[allow(dead_code)]
-fn print_playground(playground: &Playground, tail: &[Position]) {
-    for i in 0..playground.len() {
-        let mut line = String::new();
-        for j in 0..playground[i].len() {
-            let mut rope = false;
-            for (idx, (x, y)) in tail.iter().enumerate() {
-                if i == *x && j == *y {
-                    rope = true;
-                    if idx > 0 {
-                        line.push_str(idx.to_string().as_str())
-                    } else {
-                        line.push('H')
+impl Playground {
+    fn new(size: usize, number_of_knots: usize) -> Playground {
+        let mut data = Vec::from_iter((0..size).map(|_| vec![0; size]));
+        let knot_init: Position = (data.len() / 2, data[0].len() / 2);
+        let knots: Vec<Position> = Vec::from_iter(repeat(knot_init).take(number_of_knots));
+        data[knots[0].0][knots[0].1] = 1;
+        Playground { data, knots }
+    }
+
+    fn update_tail_position(&mut self, head_idx: usize, tail_idx: usize) {
+        let head = self.knots[head_idx];
+        let tail = self.knots[tail_idx];
+        let direction = (head.0 as i16 - tail.0 as i16, head.1 as i16 - tail.1 as i16);
+
+        self.knots[tail_idx] = match direction {
+            // ...    ... | .T.    ... | ...    ... | .H.    .H.
+            // T.H -> .TH | ... -> .T. | H.T -> HT. | ... -> .T.
+            // ...    ... | .H.    .H. | ...    ... | .T.    ...
+            (2, 0) => (tail.0 + 1, tail.1),
+            (0, 2) => (tail.0, tail.1 + 1),
+            (-2, 0) => (tail.0 - 1, tail.1),
+            (0, -2) => (tail.0, tail.1 - 1),
+
+            // ...    ... | .H.    .H. | ..H    ..H
+            // ..H -> .TH | ... -> .T. | ... -> .T.
+            // T..    ... | T..    ... | T..    ...
+            // ------------------------------------
+            // ...    ... | .H.    .H. | H..    H..
+            // H.. -> HT. | ... -> .T. | ... -> .T.
+            // ..T    ... | ..T    ... | ..T    ...
+            // ------------------------------------
+            // ..T    ... | ..T    ... | ..T    ...
+            // H.. -> HT. | ... -> .T. | ... -> .T.
+            // ...    ... | .H.    .H. | H..    ...
+            // ------------------------------------
+            // T..    ... | T..    ... | T..    ...
+            // ... -> .T. | ..H -> .TH | ... -> .T.
+            // .H.    .H. | ...    ... | ..H    ..H
+            (2, -1) | (1, -2) | (2, -2) => (tail.0 + 1, tail.1 - 1),
+            (-2, -1) | (-1, -2) | (-2, -2) => (tail.0 - 1, tail.1 - 1),
+            (-2, 1) | (-1, 2) | (-2, 2) => (tail.0 - 1, tail.1 + 1),
+            (2, 1) | (1, 2) | (2, 2) => (tail.0 + 1, tail.1 + 1),
+
+            // rest of the cases don't require any change
+            _ => tail,
+        }
+    }
+
+    fn update_head_position(&mut self, instruction: &String) {
+        let head = self.knots[0];
+        self.knots[0] = match instruction.as_str() {
+            "U" => (head.0 - 1, head.1),
+            "D" => (head.0 + 1, head.1),
+            "L" => (head.0, head.1 - 1),
+            "R" => (head.0, head.1 + 1),
+            _ => head,
+        }
+    }
+
+    fn move_knots(&mut self, instruction: String, steps: usize) {
+        for _ in 0..steps {
+            self.update_head_position(&instruction);
+            for i in 1..self.knots.len() {
+                self.update_tail_position(i - 1, i);
+            }
+            let last_tail = self.knots[self.knots.len() - 1];
+            self.data[last_tail.0][last_tail.1] = 1;
+        }
+    }
+
+    fn parse_line(&self, line: String) -> (String, usize) {
+        match line.split(' ').collect::<Vec<&str>>()[..] {
+            [instruction, steps] => (instruction.to_string(), steps.parse::<usize>().unwrap()),
+            _ => panic!("Unknown command"),
+        }
+    }
+
+    fn load_vec(&mut self, data: Vec<String>) {
+        for line in data {
+            let (instruction, steps) = self.parse_line(line);
+            self.move_knots(instruction, steps);
+        }
+    }
+
+    fn sum(&self) -> u32 {
+        self.data
+            .iter()
+            .map(|col| col.iter().map(|&item| item as u32).sum::<u32>())
+            .sum()
+    }
+}
+
+impl fmt::Display for Playground {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut data = String::new();
+        for i in 0..self.data.len() {
+            for j in 0..self.data[i].len() {
+                let mut rope = false;
+                for (idx, (x, y)) in self.knots.iter().enumerate() {
+                    if i == *x && j == *y {
+                        rope = true;
+                        if idx > 0 {
+                            data.push_str(idx.to_string().as_str());
+                        } else {
+                            data.push('H');
+                        }
+                        break;
                     }
-                    break;
+                }
+                if !rope {
+                    if self.data[i][j] == 1 {
+                        data.push('#');
+                    } else {
+                        data.push('.');
+                    }
                 }
             }
-            if !rope {
-                if playground[i][j] == 1 {
-                    line.push('#');
-                } else {
-                    line.push('.');
-                }
-            }
+            data.push('\n');
         }
-        println!("{}", line);
+        write!(f, "{}", data)
     }
-    println!();
-}
-
-fn parse_line(line: String) -> (Instruction, usize) {
-    match line.split(' ').collect::<Vec<&str>>()[..] {
-        [instruction, steps] => {
-            let steps_num = steps.parse::<usize>().unwrap();
-            match instruction {
-                "U" => (Instruction::Up, steps_num),
-                "D" => (Instruction::Down, steps_num),
-                "L" => (Instruction::Left, steps_num),
-                "R" => (Instruction::Right, steps_num),
-                _ => panic!("Unknown instruction"),
-            }
-        }
-        _ => panic!("Unknown command"),
-    }
-}
-
-fn calculate_tail_position(head: &Position, tail: &Position) -> Position {
-    match (head.0 as i16 - tail.0 as i16, head.1 as i16 - tail.1 as i16) {
-        // .....    .....    .....    .....
-        // .....    .....    ..H..    ..H..
-        // ...H. -> ..TH. or ..... -> ..T..
-        // .T...    .....    .T...    .....
-        // .....    .....    .....    .....
-        (2, -1) | (1, -2) => (tail.0 + 1, tail.1 - 1),
-        // .....    .....
-        // ..H..    ..H..
-        // ..... -> ..T..
-        // ..T..    .....
-        // .....    .....
-        (0, -2) => (tail.0, tail.1 - 1),
-        // .....    .....    .....    .....
-        // .....    .....    ..H..    ..H..
-        // .H... -> .HT.. or ..... -> ..T..
-        // ...T.    .....    ...T.    .....
-        // .....    .....    .....    .....
-        (-2, -1) | (-1, -2) => (tail.0 - 1, tail.1 - 1),
-        // .....    .....
-        // .....    .....
-        // .H.T. -> .HT..
-        // .....    .....
-        // .....    .....
-        (-2, 0) => (tail.0 - 1, tail.1),
-        // .....    .....    .....    .....
-        // ...T.    .....    ...T.    .....
-        // .H... -> .HT.. or ..... -> ..T..
-        // .....    .....    ..H..    ..H..
-        // .....    .....    .....    .....
-        (-2, 1) | (-1, 2) => (tail.0 - 1, tail.1 + 1),
-        // .....    .....
-        // ..T..    .....
-        // ..... -> ..T..
-        // ..H..    ..H..
-        // .....    .....
-        (0, 2) => (tail.0, tail.1 + 1),
-        // .....    .....    .....    .....
-        // .T...    .....    .T...    .....
-        // ..... -> ..T.. or ...H. -> ..TH.
-        // ..H..    ..H..    .....    .....
-        // .....    .....    .....    .....
-        (2, 1) | (1, 2) => (tail.0 + 1, tail.1 + 1),
-        // .....    .....
-        // .....    .....
-        // .T.H. -> ..TH.
-        // .....    .....
-        // .....    .....
-        (2, 0) => (tail.0 + 1, tail.1),
-        // cases where tail moves diagonally
-        (2, 2) => (tail.0 + 1, tail.1 + 1),
-        (-2, 2) => (tail.0 - 1, tail.1 + 1),
-        (2, -2) => (tail.0 + 1, tail.1 - 1),
-        (-2, -2) => (tail.0 - 1, tail.1 - 1),
-        // rest of the cases don't require any change
-        _ => *tail,
-    }
-}
-
-fn move_head(
-    playground: &mut Playground,
-    instruction: &Instruction,
-    steps: &usize,
-    tail: &mut Vec<Position>,
-) {
-    (0..*steps).for_each(|_| {
-        match instruction {
-            Instruction::Up => tail[0].0 -= 1,
-            Instruction::Down => tail[0].0 += 1,
-            Instruction::Left => tail[0].1 -= 1,
-            Instruction::Right => tail[0].1 += 1,
-        }
-        (0..(tail.len() - 1)).for_each(|i| {
-            tail[i + 1] = calculate_tail_position(&tail[i], &tail[i + 1]);
-        });
-        playground[tail[tail.len() - 1].0][tail[tail.len() - 1].1] = 1;
-    })
-}
-
-fn process_instructions(data: Vec<String>, mut playground: Playground, length: usize) -> u32 {
-    let (cols, rows) = (playground.len(), playground[0].len());
-    let mut tail = Vec::from_iter(repeat((cols / 2, rows / 2)).take(length));
-    playground[tail[0].0][tail[0].1] = 1;
-
-    for line in data {
-        let (instruction, steps) = parse_line(line);
-        move_head(&mut playground, &instruction, &steps, &mut tail);
-    }
-    playground
-        .iter()
-        .map(|col| col.iter().map(|&item| item as u32).sum::<u32>())
-        .sum()
 }
 
 pub fn run_part1(path: String) -> String {
     let data = read_file(path);
-    let playground = init_playground(1000);
-    process_instructions(data, playground, 2).to_string()
+    let mut playground = Playground::new(1000, 2);
+    playground.load_vec(data);
+    playground.sum().to_string()
 }
 
 pub fn run_part2(path: String) -> String {
     let data = read_file(path);
-    let playground = init_playground(1000);
-    process_instructions(data, playground, 10).to_string()
+    let mut playground = Playground::new(1000, 10);
+    playground.load_vec(data);
+    playground.sum().to_string()
 }
 
 #[cfg(test)]
@@ -185,8 +162,11 @@ mod tests {
             "L 5".to_string(),
             "R 2".to_string(),
         ];
-        let playground = init_playground(20);
-        assert_eq!(process_instructions(data, playground, 2), 13);
+        let mut playground = Playground::new(15, 2);
+        playground.load_vec(data);
+
+        println!("{}", &playground);
+        assert_eq!(playground.sum(), 13);
     }
 
     #[test]
@@ -201,7 +181,10 @@ mod tests {
             "L 25".to_string(),
             "U 20".to_string(),
         ];
-        let playground = init_playground(50);
-        assert_eq!(process_instructions(data, playground, 10), 36);
+        let mut playground = Playground::new(30, 10);
+        playground.load_vec(data);
+
+        println!("{}", &playground);
+        assert_eq!(playground.sum(), 36);
     }
 }
